ed9758f6458ac6050234cd012d74542aa144682c
Jeremy Yoo <jyyoo824@gmail.com>
2021-08-31 12:02:13 +0100

    first extension completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  
+  const [currentNum, setCurrentNum] = useState(1);
+  const [answer, setAnswer] = useState('');
+
+  useEffect(() => {
+    checkNum();
+  }, [currentNum])
+
+  const checkNum = () => {
+    if (currentNum % 3 === 0 && currentNum % 5 === 0) {
+      setAnswer("fizzbuzz");
+    } else if (currentNum % 3 === 0) {
+      setAnswer("fizz");
+    } else if (currentNum % 5 === 0) {
+      setAnswer("buzz");
+    } else {
+      setAnswer(currentNum);
+    }
+  }  
+
+  const handleInc = () => {
+    setCurrentNum(currentNum + 1);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h2>Current Number: {answer}</h2>
+      <button value={currentNum} onClick={handleInc}>Play!</button>
+      <button >Fizz</button>
+      <button >Buzz</button>
+      <button >FizzBuzz</button>
+    
+    </>
   );
 }
 
